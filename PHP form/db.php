fe6f101bf3d4a287967a68b6208d272d2c32d786
Phil Hallberg <philh928@gmail.com>
2018-10-04 23:25:13 -0500

    rewrite of PHP

--- PHP form/db.php
+++ PHP form/db.php
@@ -1,14 +1,48 @@
 <?php
-        mysql_connect("localhost", "root" , "" ); 
-        mysql_select_db("demo1"); // your database name is added here 
-         if (isset ($_POST['submit'])) 
-             { 
-                $email=$_POST['email']};
-                $pass=$_POST['pass'];
-                $query-"insert into login" (email,password) values ('$email','$pass')";
-                 if(mysql_query ($query)) //run the query 
-                 { 
-                     echo" your date has been recorded" ; 
-                    } 
+require "/var/www/utils/variables.php";
+require "/var/www/db/db_config.php";
+
+session_start();
+
+$_SESSION['authenticated'] = false;
+$goto = $_SESSION['goto'];
+
+
+
+$cmd = $_POST["cmd"];
+
+
+if ($cmd == "signIn")
+{
+   $username = $_POST["username"];
+   $password = $_POST["password"];
+   $runFunc = signIn($username, $password);
+   die($runFunc);
+} 
+
+    function signIn($username, $password) 
+    {
+        $pdo = pdo_connect();
+        $exec = array(":username"=>$username);
+        $sql = "SELECT * FROM `users` WHERE `username` = :username";
+        $result = pdo_select($sql,$exec);
+            $user = $result[0]["username"];
+            $pass = $result[0]["password"];  
+
+        if($pass==md5($password)){
+            if($user == $username) {
+                logUser($user);
+                $_SESSION["authenticated"] = true;
+                $_SESSION["username"] = $user;
+            } 
+            
+            if($_SESSION["authenticated"] = true) {
+                if(isset($goto)){
+                    header($goto);
+                } else {
+                    header('Location: index.php');
                 }
-                ?>
+            }
+        }
+    }
+?>
